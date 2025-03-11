230be6042373832beaeec20c0ec0e22e845c6edc
ErnarKBTU <123051594+ErnarM04@users.noreply.github.com>
2025-03-12 01:06:32 +0500

    New Project

---
+++ app/src/main/java/com/example/startups/Data.kt
@@ -0,0 +1,10 @@
+package com.example.startups
+
+import androidx.lifecycle.ViewModel
+
+class Data: ViewModel() {
+    protected lateinit var name: String
+    protected lateinit var email: String
+    protected lateinit var password: String
+
+}
